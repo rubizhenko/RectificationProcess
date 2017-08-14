28ea9041825a8a474dcf74609dcd0a1a0881bc9b
Dmytro Rubizhenko <d.rubizhenko@gmail.com>
2017-08-15 00:24:00 +0300

    Created basic class for evaluate static params

---
+++ RectificationProcess/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace RectificationProcess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(752, 475);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.chart1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(744, 449);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(6, 6);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(732, 437);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(776, 499);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ RectificationProcess/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RectificationProcess
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string progPath = Environment.CurrentDirectory;
+            RectificationProcess process = new RectificationProcess();
+            process.StaticT2otFbinar1.;
+            process.StaticT2otFvod();
+            process.StaticT2otTbinar1();
+            process.StaticT2otTvod();
+            process.StaticTp1opFb();
+            process.StaticTp1opFd();
+            process.StaticTp1opFf();
+            process.StaticTp1opTb();
+            process.StaticTp1opTd();
+            process.StaticTp1opTp2();
+            process.StaticTp2otFp();
+            process.StaticTp2otFpara();
+            process.StaticTp2otTp1();
+        }
+        public class RectificationProcess
+        {
+            //Параметри дистиляту
+            private double Fdist = 1530, Cdist = 3.670, Tdist = 293;
+            //Параметри бінарної суміші
+            private double Fbinar1 = 540, Cbinar1 = 3.260, Tbinar1 = 342;
+            //Параметри дистиляту
+            private double Fvoda = 650, Cvoda = 4.200, Tvoda = 293;
+            //Параметри конденсату
+            private double Fkondvoda = 650, Tkondvoda = 4.200, Ckondvoda = 308;
+            //Параметри бінарної суміші
+            private double Fbinar2 = 540, Cbinar2 = 3.260, Tbinar2 = 312;
+            //Параметри флегми
+            private double Cflegmy = 3.260, Tflegmy = 312, Fflegmy = 320;
+            //Параметри кубового залишку
+            private double Fcub = 1110, Ccub = 2.900, Tcub = 359;
+            //Параметри рециркуляту
+            private double Frecur1 = 960, Crecur1 = 2.900, Trecur1 = 359;
+            //Параметри рециркуляту
+            private double Frecur2 = 960, Crecur2 = 2.900, Trecur2 = 383;
+            //Параметри пари
+            private double Fpara = 141, Ppara = 20.101, Ipara = 2800;
+            //Параметри конденсованої пари
+            private double Fkondpara = 2834, Ckondpara = 4.200, Tkondpara = 297;
+
+            private double q = 0.225;   //для дефлегматора
+            private double q1 = 2861;   //для кип'ятильника
+            private double k = 512;     //для дефлегматора
+            private double s = 20;      //для дефлегматора
+            private double k1 = 1308;   //для кип'ятильника
+            private double s1 = 25;     //для кип'ятильника
+            private double k2 = 168.143;   //для колони
+            private double s2 = 35;     //для колони
+            private double q2 = 0.393;  //для колони
+
+            //для дефлегматора
+            double[] T_binar2_1 = new double[9];
+            double[] T_binar2_2 = new double[9];
+            double[] T_binar2_3 = new double[9];
+            double[] T_binar2_4 = new double[9];
+
+            //для кип'ятильника
+            double[] T_recur2_1 = new double[9];
+            double[] T_recur2_2 = new double[9];
+            double[] T_recur2_3 = new double[9];
+
+            //для колони
+            double[] T_recur1_1 = new double[9];
+            double[] T_recur1_2 = new double[9];
+            double[] T_recur1_3 = new double[9];
+            double[] T_recur1_4 = new double[9];
+            double[] T_recur1_5 = new double[9];
+            double[] T_recur1_6 = new double[9];
+            
+            double K1 = 0, K2 = 0, K3 = 0, K4 = 0, K5 = 0, K6 = 0, K7 = 0, K8 = 0, K9 = 0, K10 = 0, K11 = 0, K12 = 0, K13 = 0;
+
+            //Формули для розрахунку статичних характеристик дефлегматора
+            public Tuple<double[], double> StaticT2otTbinar1()
+            {
+                Tbinar1 = 332;
+                for (int i = 1; i <= 9; i++)
+                {
+                    Tbinar1 = Tbinar1 + 2;
+                    T_binar2_1[i - 1] = (0.8 * Fbinar1 * Tbinar1 * Cbinar1 + Fvoda * Tvoda * Cvoda - Fkondvoda * Ckondvoda * Tkondvoda + 0.8 * Fbinar1 * Tbinar1 * Cbinar1 * q) / (Fbinar1 * Cbinar1);
+                    T_binar2_1[i - 1] = Math.Round(T_binar2_1[i - 1], 2);
+                }
+                K1 = (T_binar2_1[1] - T_binar2_1[2]) / -2;
+                Tbinar1 = 342;
+                return Tuple.Create(T_binar2_1, K1);
+            }
+            public void StaticT2otFbinar1()
+            {
+                Fbinar1 = 530;
+                for (int i = 1; i <= 9; i++)
+                {
+                    Fbinar1 = Fbinar1 + 2;
+                    T_binar2_2[i - 1] = (0.8 * Fbinar1 * Tbinar1 * Cbinar1 + Fvoda * Tvoda * Cvoda - Fkondvoda * Ckondvoda * Tkondvoda + 0.8 * Fbinar1 * Tbinar1 * Cbinar1 * q) / (Fbinar1 * Cbinar1);
+                    T_binar2_2[i - 1] = Math.Round(T_binar2_2[i - 1], 2);
+                }
+                K2 = (T_binar2_2[1] - T_binar2_2[2]) / -2;
+                Fbinar1 = 540;
+            }
+            public void StaticT2otFvod()
+            {
+                Fvoda = 640;
+                for (int i = 1; i <= 9; i++)
+                {
+                    Fvoda = Fvoda + 2;
+                    T_binar2_3[i - 1] = (Fbinar1 * Tbinar1 * Cbinar1 + k * s * ((Fvoda * Tvoda * Cvoda) / (Fvoda * Cvoda + k * s)) - 0.2 * (Fbinar1 * Tbinar1 * Cbinar1 * q + Fbinar1 * Tbinar1 * Cbinar1) + Fbinar1 * Tbinar1 * Cbinar1 * q) / (Fbinar1 * Cbinar1 + k * s - k * s * ((k * s) / (Fvoda * Cvoda + k * s)));
+                    T_binar2_3[i - 1] = Math.Round(T_binar2_3[i - 1], 2);
+                }
+                K3 = (T_binar2_3[1] - T_binar2_3[2]) / -2;
+                Fvoda = 650;
+            }
+            public void StaticT2otTvod()
+            {
+                Tvoda = 283;
+                for (int i = 1; i <= 9; i++)
+                {
+                    Tvoda = Tvoda + 2;
+                    T_binar2_4[i - 1] = (Fbinar1 * Tbinar1 * Cbinar1 + k * s * ((Fvoda * Tvoda * Cvoda) / (Fvoda * Cvoda + k * s)) - 0.2 * (Fbinar1 * Tbinar1 * Cbinar1 * q + Fbinar1 * Tbinar1 * Cbinar1) + Fbinar1 * Tbinar1 * Cbinar1 * q) / (Fbinar1 * Cbinar1 + k * s - k * s * ((k * s) / (Fvoda * Cvoda + k * s)));
+                    T_binar2_4[i - 1] = Math.Round(T_binar2_4[i - 1], 2);
+                }
+                K4 = (T_binar2_4[1] - T_binar2_4[2]) / -2;
+                Tvoda = 293;
+            }
+
+            //Формули для розрахунку статичних характеристик кип'ятильника
+            public void StaticTp2otFp()
+            {
+                Frecur1 = 950;
+                for (int i = 1; i <= 9; i++)
+                {
+                    Frecur1 = Frecur1 + 2;
+                    T_recur2_1[i - 1] = (Frecur1 * Crecur1 * Trecur1 + 0.8 * (Fpara * Ipara * Ppara) - Fpara * Ppara * Tkondpara * Ckondpara - Frecur1 * q1) / (Frecur1 * Crecur1);
+                    T_recur2_1[i - 1] = Math.Round(T_recur2_1[i - 1], 2);
+                }
+                //Перевірити
+                K5 = (T_recur2_1[1] - T_recur2_1[2]) / -2;
+                Frecur1 = 960;
+            }
+            public void StaticTp2otTp1()
+            {
+                Trecur1 = 349;
+                for (int i = 1; i <= 9; i++)
+                {
+                    Trecur1 += 2;
+                    T_recur2_2[i - 1] = (Frecur1 * Crecur1 * Trecur1 + 0.8 * (Fpara * Ipara * Ppara) - Fpara * Ppara * Tkondpara * Ckondpara - Frecur1 * q1) / (Frecur1 * Crecur1);
+                    T_recur2_2[i - 1] = Math.Round(T_recur2_2[i - 1], 2);
+                }
+                K6 = (T_recur2_2[1] - T_recur2_2[2]) / -2;
+                Trecur1 = 359;
+            }
+            public void StaticTp2otFpara()
+            {
+                Fpara = 131;
+                for (int i = 1; i <= 9; i++)
+                {
+                    Fpara += 2;
+                    T_recur2_3[i - 1] = (-(Frecur1 * Crecur1 * Trecur1 - Frecur1 * q1 - k1 * s1 * (-0.8 * Fpara * Ipara * Ppara / (k1 * s1 - Fpara * Ppara * Ckondpara))) / (-Frecur1 * Crecur1 + k1 * s1 - k1 * s1 * (k1 * s1 / (k1 * s1 - Fpara * Ppara * Ckondpara))));
+                    T_recur2_3[i - 1] = Math.Round(T_recur2_3[i - 1], 2);
+                }
+                //перевірити
+                K7 = (T_recur2_3[1] - T_recur2_3[2]) / -2;
+                Fpara = 141;
+            }
+
+            //Формули для розрахунку статичних характеристик колони
+            public void StaticTp1opFd()
+            {
+                Fdist = 1505;
+                for (int i = 1; i <= 9; i++)
+                {
+                    Fdist = Fdist + 5;
+                    T_recur1_1[i - 1] = (Fdist * Cdist * Tdist + 0.8 * Frecur1 * Trecur2 * Crecur1 - (Fdist * Cdist * Tdist + Frecur1 * Trecur2 * Crecur1) * q2 + k2 * s2 * (Fbinar1 * Tbinar1 * Cbinar1 / (Fflegmy * Cflegmy - k2 * s2))) / (Fcub * Ccub + k2 * s2 - k2 * s2 * ((-k2 * s2) / (Fflegmy * Cflegmy - k2 * s2)));
+                    T_recur1_1[i - 1] = Math.Round(T_recur1_1[i - 1], 2);
+                }
+                K8 = (T_recur1_1[1] - T_recur1_1[2]) / -5;
+                Fdist = 1530;
+            }
+            public void StaticTp1opTd()
+            {
+                Tdist = 278;
+                for (int i = 1; i <= 9; i++)
+                {
+                    Tdist = Tdist + 3;
+                    T_recur1_2[i - 1] = (Fdist * Cdist * Tdist + 0.8 * Frecur1 * Trecur2 * Crecur1 - (Fdist * Cdist * Tdist + Frecur1 * Trecur2 * Crecur1) * q2 + k2 * s2 * (Fbinar1 * Tbinar1 * Cbinar1 / (Fflegmy * Cflegmy - k2 * s2))) / (Fcub * Ccub + k2 * s2 - k2 * s2 * ((-k2 * s2) / (Fflegmy * Cflegmy - k2 * s2)));
+                    T_recur1_2[i - 1] = Math.Round(T_recur1_2[i - 1], 2);
+                }
+                K9 = (T_recur1_2[1] - T_recur1_2[2]) / -3;
+                Tdist = 293;
+            }
+            public void StaticTp1opTp2()
+            {
+                Trecur2 = 378;
+                for (int i = 1; i <= 9; i++)
+                {
+                    Trecur2 = Trecur2 + 3;
+                    T_recur1_3[i - 1] = (Fdist * Cdist * Tdist + 0.8 * Frecur1 * Trecur2 * Crecur1 - (Fdist * Cdist * Tdist + Frecur1 * Trecur2 * Crecur1) * q2 + k2 * s2 * (Fbinar1 * Tbinar1 * Cbinar1 / (Fflegmy * Cflegmy - k2 * s2))) / (Fcub * Ccub + k2 * s2 - k2 * s2 * ((-k2 * s2) / (Fflegmy * Cflegmy - k2 * s2)));
+                    T_recur1_3[i - 1] = Math.Round(T_recur1_3[i - 1], 2);
+                }
+                K10 = (T_recur1_3[1] - T_recur1_3[2]) / -3;
+                Trecur2 = 383;
+            }
+            public void StaticTp1opFf()
+            {
+                Fflegmy = 295;
+                for (int i = 1; i <= 9; i++)
+                {
+                    Fflegmy = Fflegmy + 5;
+                    T_recur1_4[i - 1] = (Fdist * Cdist * Tdist + 0.8 * Frecur1 * Trecur2 * Crecur1 - (Fdist * Cdist * Tdist + Frecur1 * Trecur2 * Crecur1) * q2 + k2 * s2 * (Fbinar1 * Tbinar1 * Cbinar1 / (Fflegmy * Cflegmy - k2 * s2))) / (Fcub * Ccub + k2 * s2 - k2 * s2 * ((-k2 * s2) / (Fflegmy * Cflegmy - k2 * s2)));
+                    T_recur1_4[i - 1] = Math.Round(T_recur1_4[i - 1], 2);
+                }
+                K11 = (T_recur1_4[1] - T_recur1_4[2]) / -5;
+                Fflegmy = 320;
+            }
+            public void StaticTp1opFb()
+            {
+                Fbinar1 = 515;
+                for (int i = 1; i <= 9; i++)
+                {
+                    Fbinar1 = Fbinar1 + 5;
+                    T_recur1_5[i - 1] = (Fdist * Cdist * Tdist + 0.8 * Frecur1 * Trecur2 * Crecur1 - (Fdist * Cdist * Tdist + Frecur1 * Trecur2 * Crecur1) * q2 + k2 * s2 * (Fbinar1 * Tbinar1 * Cbinar1 / (Fflegmy * Cflegmy - k2 * s2))) / (Fcub * Ccub + k2 * s2 - k2 * s2 * ((-k2 * s2) / (Fflegmy * Cflegmy - k2 * s2)));
+                    T_recur1_5[i - 1] = Math.Round(T_recur1_5[i - 1], 2);
+                }
+                K12 = (T_recur1_5[1] - T_recur1_5[2]) / -5;
+                Fbinar1 = 540;
+            }
+            public void StaticTp1opTb()
+            {
+                Tbinar1 = 327;
+                for (int i = 1; i <= 9; i++)
+                {
+                    Tbinar1 = Tbinar1 + 3;
+                    T_recur1_6[i - 1] = (Fdist * Cdist * Tdist + 0.8 * Frecur1 * Trecur2 * Crecur1 - (Fdist * Cdist * Tdist + Frecur1 * Trecur2 * Crecur1) * q2 + k2 * s2 * (Fbinar1 * Tbinar1 * Cbinar1 / (Fflegmy * Cflegmy - k2 * s2))) / (Fcub * Ccub + k2 * s2 - k2 * s2 * ((-k2 * s2) / (Fflegmy * Cflegmy - k2 * s2)));
+                    T_recur1_6[i - 1] = Math.Round(T_recur1_6[i - 1], 2);
+                }
+                K13 = (T_recur1_6[1] - T_recur1_6[2]) / -3;
+                Tbinar1 = 342;
+            }
+        }
+    }
+}

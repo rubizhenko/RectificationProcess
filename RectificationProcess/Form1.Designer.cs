188d9f164f1303b791950f88ddbbd96eeaa221a1
Dmytro Rubizhenko <d.rubizhenko@gmail.com>
2017-08-18 00:32:24 +0300

    Added static charts for process units

--- RectificationProcess/Form1.Designer.cs
+++ RectificationProcess/Form1.Designer.cs
@@ -28,22 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.процесToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.статичніХарактеристикиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.дефлегматорToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.кипятильникToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ректифікаційнаКолонаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.динамічніХарактеристикиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.дефлегматорToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.кипятильникToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ректифікаційнаКолонаToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Location = new System.Drawing.Point(12, 38);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(752, 475);
@@ -63,17 +74,17 @@
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
             this.chart1.Location = new System.Drawing.Point(6, 6);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series1.MarkerBorderWidth = 3;
-            series1.MarkerColor = System.Drawing.Color.Red;
-            series1.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.MarkerBorderWidth = 3;
+            series3.MarkerColor = System.Drawing.Color.Red;
+            series3.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
             this.chart1.Size = new System.Drawing.Size(732, 437);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
@@ -88,18 +99,100 @@
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.процесToolStripMenuItem,
+            this.статичніХарактеристикиToolStripMenuItem,
+            this.динамічніХарактеристикиToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1096, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // процесToolStripMenuItem
+            // 
+            this.процесToolStripMenuItem.Name = "процесToolStripMenuItem";
+            this.процесToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.процесToolStripMenuItem.Text = "Процес";
+            // 
+            // статичніХарактеристикиToolStripMenuItem
+            // 
+            this.статичніХарактеристикиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.дефлегматорToolStripMenuItem,
+            this.кипятильникToolStripMenuItem,
+            this.ректифікаційнаКолонаToolStripMenuItem});
+            this.статичніХарактеристикиToolStripMenuItem.Name = "статичніХарактеристикиToolStripMenuItem";
+            this.статичніХарактеристикиToolStripMenuItem.Size = new System.Drawing.Size(156, 20);
+            this.статичніХарактеристикиToolStripMenuItem.Text = "Статичні характеристики";
+            // 
+            // дефлегматорToolStripMenuItem
+            // 
+            this.дефлегматорToolStripMenuItem.Name = "дефлегматорToolStripMenuItem";
+            this.дефлегматорToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.дефлегматорToolStripMenuItem.Text = "Дефлегматор";
+            this.дефлегматорToolStripMenuItem.Click += new System.EventHandler(this.дефлегматорToolStripMenuItem_Click);
+            // 
+            // кипятильникToolStripMenuItem
+            // 
+            this.кипятильникToolStripMenuItem.Name = "кипятильникToolStripMenuItem";
+            this.кипятильникToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.кипятильникToolStripMenuItem.Text = "Кип\'ятильник";
+            this.кипятильникToolStripMenuItem.Click += new System.EventHandler(this.кипятильникToolStripMenuItem_Click);
+            // 
+            // ректифікаційнаКолонаToolStripMenuItem
+            // 
+            this.ректифікаційнаКолонаToolStripMenuItem.Name = "ректифікаційнаКолонаToolStripMenuItem";
+            this.ректифікаційнаКолонаToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.ректифікаційнаКолонаToolStripMenuItem.Text = "Ректифікаційна колона";
+            this.ректифікаційнаКолонаToolStripMenuItem.Click += new System.EventHandler(this.ректифікаційнаКолонаToolStripMenuItem_Click);
+            // 
+            // динамічніХарактеристикиToolStripMenuItem
+            // 
+            this.динамічніХарактеристикиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.дефлегматорToolStripMenuItem1,
+            this.кипятильникToolStripMenuItem1,
+            this.ректифікаційнаКолонаToolStripMenuItem1});
+            this.динамічніХарактеристикиToolStripMenuItem.Name = "динамічніХарактеристикиToolStripMenuItem";
+            this.динамічніХарактеристикиToolStripMenuItem.Size = new System.Drawing.Size(165, 20);
+            this.динамічніХарактеристикиToolStripMenuItem.Text = "Динамічні характеристики";
+            // 
+            // дефлегматорToolStripMenuItem1
+            // 
+            this.дефлегматорToolStripMenuItem1.Name = "дефлегматорToolStripMenuItem1";
+            this.дефлегматорToolStripMenuItem1.Size = new System.Drawing.Size(202, 22);
+            this.дефлегматорToolStripMenuItem1.Text = "Дефлегматор";
+            // 
+            // кипятильникToolStripMenuItem1
+            // 
+            this.кипятильникToolStripMenuItem1.Name = "кипятильникToolStripMenuItem1";
+            this.кипятильникToolStripMenuItem1.Size = new System.Drawing.Size(202, 22);
+            this.кипятильникToolStripMenuItem1.Text = "Кип\'ятильник";
+            // 
+            // ректифікаційнаКолонаToolStripMenuItem1
+            // 
+            this.ректифікаційнаКолонаToolStripMenuItem1.Name = "ректифікаційнаКолонаToolStripMenuItem1";
+            this.ректифікаційнаКолонаToolStripMenuItem1.Size = new System.Drawing.Size(202, 22);
+            this.ректифікаційнаКолонаToolStripMenuItem1.Text = "Ректифікаційна колона";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(776, 499);
+            this.ClientSize = new System.Drawing.Size(1096, 587);
             this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Процес ректифікації";
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -109,6 +202,16 @@
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem процесToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem статичніХарактеристикиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem дефлегматорToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem кипятильникToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ректифікаційнаКолонаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem динамічніХарактеристикиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem дефлегматорToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem кипятильникToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem ректифікаційнаКолонаToolStripMenuItem1;
     }
 }
 
--- RectificationProcess/Form1.cs
+++ RectificationProcess/Form1.cs
@@ -16,12 +16,13 @@ namespace RectificationProcess
         string[] tabsForDeflegmator = { "Tb2(Tb1)", "Tb2(Fb1)", "Tb2(Fv)", "Tb2(Tv)" };
         string[] tabsForBoiler = { "Tr2(Fr1)", "Tr2(Tr1)", "Tr2(Fp)" };
         string[] tabsForRectifCol = { "Tr1(Fdist)", "Tr1(Tdist)", "Tr1(Tr2)", "Tr1(Ffleg)", "Tr1(Fb)", "Tr1(Tb)" };
-
+        RectificationProcess process = new RectificationProcess();
+        string currentChart = "Defleg";
         public Form1()
         {
             InitializeComponent();
             string progPath = Environment.CurrentDirectory;
-            RectificationProcess process = new RectificationProcess();
+            
             process.StaticT2otFbinar1();
             process.StaticT2otFvod();
             //double k = process.StaticT2otTbinar1().Item1[0];
@@ -38,7 +39,7 @@ namespace RectificationProcess
             process.StaticTp2otTp1();
             process.AddTabsForTabControl(tabControl1, tabsForDeflegmator);
 
-            process.DrawStaticFunction(chart1, 293, process.StaticTp1opTd().Item1, "Tdist, K", "Trecur1, K");
+            
         }
         public class RectificationProcess
         {
@@ -96,7 +97,37 @@ namespace RectificationProcess
             
             double K1 = 0, K2 = 0, K3 = 0, K4 = 0, K5 = 0, K6 = 0, K7 = 0, K8 = 0, K9 = 0, K10 = 0, K11 = 0, K12 = 0, K13 = 0;
 
-            
+            public double Fdist1 { get => Fdist; set => Fdist = value; }
+            public double Cdist1 { get => Cdist; set => Cdist = value; }
+            public double Tdist1 { get => Tdist; set => Tdist = value; }
+            public double Fbinar11 { get => Fbinar1; set => Fbinar1 = value; }
+            public double Cbinar11 { get => Cbinar1; set => Cbinar1 = value; }
+            public double Tbinar11 { get => Tbinar1; set => Tbinar1 = value; }
+            public double Fvoda1 { get => Fvoda; set => Fvoda = value; }
+            public double Cvoda1 { get => Cvoda; set => Cvoda = value; }
+            public double Tvoda1 { get => Tvoda; set => Tvoda = value; }
+            public double Fkondvoda1 { get => Fkondvoda; set => Fkondvoda = value; }
+            public double Tkondvoda1 { get => Tkondvoda; set => Tkondvoda = value; }
+            public double Ckondvoda1 { get => Ckondvoda; set => Ckondvoda = value; }
+            public double Fbinar21 { get => Fbinar2; set => Fbinar2 = value; }
+            public double Cbinar21 { get => Cbinar2; set => Cbinar2 = value; }
+            public double Tbinar21 { get => Tbinar2; set => Tbinar2 = value; }
+            public double Cflegmy1 { get => Cflegmy; set => Cflegmy = value; }
+            public double Tflegmy1 { get => Tflegmy; set => Tflegmy = value; }
+            public double Fflegmy1 { get => Fflegmy; set => Fflegmy = value; }
+            public double Fcub1 { get => Fcub; set => Fcub = value; }
+            public double Ccub1 { get => Ccub; set => Ccub = value; }
+            public double Tcub1 { get => Tcub; set => Tcub = value; }
+            public double Frecur11 { get => Frecur1; set => Frecur1 = value; }
+            public double Crecur11 { get => Crecur1; set => Crecur1 = value; }
+            public double Trecur11 { get => Trecur1; set => Trecur1 = value; }
+            public double Frecur21 { get => Frecur2; set => Frecur2 = value; }
+            public double Crecur21 { get => Crecur2; set => Crecur2 = value; }
+            public double Trecur21 { get => Trecur2; set => Trecur2 = value; }
+            public double Fpara1 { get => Fpara; set => Fpara = value; }
+            public double Fkondpara1 { get => Fkondpara; set => Fkondpara = value; }
+
+
 
             //Формули для розрахунку статичних характеристик дефлегматора
             public Tuple<double[], double> StaticT2otTbinar1()
@@ -159,7 +190,7 @@ namespace RectificationProcess
                 for (int i = 1; i <= 9; i++)
                 {
                     Frecur1 = Frecur1 + 2;
-                    T_recur2_1[i - 1] = (Frecur1 * Crecur1 * Trecur1 + 0.8 * (Fpara * Ipara * Ppara) - Fpara * Ppara * Tkondpara * Ckondpara - Frecur1 * q1) / (Frecur1 * Crecur1);
+                    T_recur2_1[i - 1] = (Frecur1 * Crecur1 * Trecur1 + 0.8 * (Fpara1 * Ipara * Ppara) - Fpara1 * Ppara * Tkondpara * Ckondpara - Frecur1 * q1) / (Frecur1 * Crecur1);
                     T_recur2_1[i - 1] = Math.Round(T_recur2_1[i - 1], 2);
                 }
                 //Перевірити
@@ -173,7 +204,7 @@ namespace RectificationProcess
                 for (int i = 1; i <= 9; i++)
                 {
                     Trecur1 += 2;
-                    T_recur2_2[i - 1] = (Frecur1 * Crecur1 * Trecur1 + 0.8 * (Fpara * Ipara * Ppara) - Fpara * Ppara * Tkondpara * Ckondpara - Frecur1 * q1) / (Frecur1 * Crecur1);
+                    T_recur2_2[i - 1] = (Frecur1 * Crecur1 * Trecur1 + 0.8 * (Fpara1 * Ipara * Ppara) - Fpara1 * Ppara * Tkondpara * Ckondpara - Frecur1 * q1) / (Frecur1 * Crecur1);
                     T_recur2_2[i - 1] = Math.Round(T_recur2_2[i - 1], 2);
                 }
                 K6 = (T_recur2_2[1] - T_recur2_2[2]) / -2;
@@ -182,16 +213,16 @@ namespace RectificationProcess
             }
             public Tuple<double[], double> StaticTp2otFpara()
             {
-                Fpara = 131;
+                Fpara1 = 131;
                 for (int i = 1; i <= 9; i++)
                 {
-                    Fpara += 2;
-                    T_recur2_3[i - 1] = (-(Frecur1 * Crecur1 * Trecur1 - Frecur1 * q1 - k1 * s1 * (-0.8 * Fpara * Ipara * Ppara / (k1 * s1 - Fpara * Ppara * Ckondpara))) / (-Frecur1 * Crecur1 + k1 * s1 - k1 * s1 * (k1 * s1 / (k1 * s1 - Fpara * Ppara * Ckondpara))));
+                    Fpara1 += 2;
+                    T_recur2_3[i - 1] = (-(Frecur1 * Crecur1 * Trecur1 - Frecur1 * q1 - k1 * s1 * (-0.8 * Fpara1 * Ipara * Ppara / (k1 * s1 - Fpara1 * Ppara * Ckondpara))) / (-Frecur1 * Crecur1 + k1 * s1 - k1 * s1 * (k1 * s1 / (k1 * s1 - Fpara1 * Ppara * Ckondpara))));
                     T_recur2_3[i - 1] = Math.Round(T_recur2_3[i - 1], 2);
                 }
                 //перевірити
                 K7 = (T_recur2_3[1] - T_recur2_3[2]) / -2;
-                Fpara = 141;
+                Fpara1 = 141;
                 return Tuple.Create(T_recur2_3, K7);
             }
 
@@ -283,8 +314,8 @@ namespace RectificationProcess
                 }
                 myChart.ChartAreas[0].AxisX.Title = XTitle;
                 myChart.ChartAreas[0].AxisY.Title = YTitle;
-                myChart.ChartAreas[0].AxisY.Minimum = YValues[0];
-                myChart.ChartAreas[0].AxisY.Maximum = YValues[YValues.Length-1];
+                myChart.ChartAreas[0].AxisY.Minimum = YValues.Min();
+                myChart.ChartAreas[0].AxisY.Maximum = YValues.Max();
 
                 int xStart = (int)(XValueCenter - XValueCenter * 0.05);
                 int xEnd = (int)(XValueCenter + XValueCenter * 0.05);
@@ -312,10 +343,79 @@ namespace RectificationProcess
 
         private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            tabControl1.TabPages[tabControl1.SelectedIndex].Controls.Clear();
-            tabControl1.TabPages[tabControl1.SelectedIndex].Controls.Add(chart1);
+            if (tabControl1.SelectedTab!=null)
+            {
+                tabControl1.TabPages[tabControl1.SelectedIndex].Controls.Clear();
+                switch (tabControl1.SelectedTab.Text)
+                {
+                    //deflegmator tabs
+                    case "Tb2(Tb1)":
+                        process.DrawStaticFunction(chart1, process.Tbinar11, process.StaticT2otTbinar1().Item1, "Tbinar1, K", "Tbinar2, K");
+                        break;
+                    case "Tb2(Fb1)":
+                        process.DrawStaticFunction(chart1, process.Fbinar11, process.StaticT2otFbinar1().Item1, "Fbinar1, K", "Tbinar2, K");
+                        break;
+                    case "Tb2(Fv)":
+                        process.DrawStaticFunction(chart1, process.Fvoda1, process.StaticT2otFvod().Item1, "Fvoda, m^3/c", "Tbinar2, K");
+                        break;
+                    case "Tb2(Tv)":
+                        process.DrawStaticFunction(chart1, process.Tvoda1, process.StaticT2otTvod().Item1, "Tvoda, K", "Tbinar2, K");
+                        break;
 
+                    //boiler tabs
+                    case "Tr2(Fr1)":
+                        process.DrawStaticFunction(chart1, process.Frecur11, process.StaticTp2otFp().Item1, "Frecur1, m^3/c", "Trecur2, K");
+                        break;
+                    case "Tr2(Tr1)":
+                        process.DrawStaticFunction(chart1, process.Trecur11, process.StaticTp2otTp1().Item1, "Trecur1, K", "Trecur2, K");
+                        break;
+                    case "Tr2(Fp)":
+                        process.DrawStaticFunction(chart1, process.Fpara1, process.StaticTp2otFp().Item1, "Fpara, m^3/c", "Trecur2, K");
+                        break;
+
+                    //rectification column tabs
+                    case "Tr1(Fdist)":
+                        process.DrawStaticFunction(chart1, process.Fdist1, process.StaticTp1opFd().Item1, "Fdist, m^3/c", "Trecur1, K");
+                        break;
+                    case "Tr1(Tdist)":
+                        process.DrawStaticFunction(chart1, process.Tdist1, process.StaticTp1opTd().Item1, "Tdist, K", "Trecur1, K");
+                        break;
+                    case "Tr1(Tr2)":
+                        process.DrawStaticFunction(chart1, process.Trecur21, process.StaticTp1opTp2().Item1, "Trecur2, K", "Trecur1, K");
+                        break;
+                    case "Tr1(Ffleg)":
+                        process.DrawStaticFunction(chart1, process.Fflegmy1, process.StaticTp1opFf().Item1, "Fflegmy, m^3/c", "Trecur1, K");
+                        break;
+                    case "Tr1(Fb)":
+                        process.DrawStaticFunction(chart1, process.Fbinar11, process.StaticTp1opFb().Item1, "Fbinar, m^3/c", "Trecur1, K");
+                        break;
+                    case "Tr1(Tb)":
+                        process.DrawStaticFunction(chart1, process.Tbinar11, process.StaticTp1opTb().Item1, "Fbinar, m^3/c", "Trecur1, K");
+                        break;
+
+                    //default chart for deflegmator
+                    default:
+                        process.DrawStaticFunction(chart1, process.Tbinar11, process.StaticT2otTbinar1().Item1, "Tbinar1, K", "Tbinar2, K");
+                        break;
+                }
+                tabControl1.TabPages[tabControl1.SelectedIndex].Controls.Add(chart1);
+            }
+            
         }
-       
+
+        private void дефлегматорToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            process.AddTabsForTabControl(tabControl1, tabsForDeflegmator);
         }
+
+        private void кипятильникToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            process.AddTabsForTabControl(tabControl1, tabsForBoiler);
+        }
+
+        private void ректифікаційнаКолонаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            process.AddTabsForTabControl(tabControl1, tabsForRectifCol);
+        }
+    }
 }
